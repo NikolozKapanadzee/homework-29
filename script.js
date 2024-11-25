ad86328d971ba01ed5b985e6e1bc038e0c12e791
NikolozKapanadzee <128920612+NikolozKapanadzee@users.noreply.github.com>
2024-11-25 05:04:26 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+
+// //exe 1
+
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     introduce() {
+//         console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+//     }
+// }
+// let myProfile = new Person("guja", 20);
+// myProfile.introduce();
+
+
+
+//exe 2
+
+//ver gavige es
+
+
+
+
+//exe 3
+
+
+// class Animal {
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     speak() {
+//         console.log(`${this.name} რაღაც ხმას გამოსცემს`);
+//     }
+// }
+// class Dog extends Animal {
+//     constructor(breed) {
+//         super(breed);
+//         this.breed = breed;
+
+//     }
+//     bark() {
+//         console.log(`${this.breed} ყეფს`);
+//     }
+// }
+// let myDog = new Dog("ლაბრადორი")
+// myDog.bark();
+
+
+
+//exe 4 
+
+// didi minusi amas
+
+
+
+//exe 5 
+
+
+// class Vehicle {
+//     constructor(brand, speed) {
+//         this.brand = brand;
+//         this.speed = speed;
+//     }
+//     describe() {
+//         console.log(`es aris ${this.brand} da mas aqvs ${this.speed} sichqare`);
+//     }
+// }
+// class Car extends Vehicle {
+//     constructor(model) {
+//         super(model);
+//         this.model = model;
+
+//     }
+//     describe() {
+//         console.log(`modelia ${this.model}`);
+//     }
+// }
+// let carInfo = new Car("M")
+// carInfo.describe();
+
+
+
+// exe 6,7,8,9,10 
+
+
+// amasac didi -
